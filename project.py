e257830e543d180c8e78cfe1a9d9c1ef6291d4c4
WeijiaMa123 <31965430+WeijiaMa123@users.noreply.github.com>
2017-11-28 14:59:45 -0500

    change2

--- project.py
+++ project.py
@@ -4,6 +4,15 @@ gsize = input("How big do you want your group to be")
 def cgroups(size,list):
     random.shuffle(list)
     if size == "2":
-        return list 
+        count=0
+        for i in list:
+##         for i in range(len(list)):
+            if count < len(list)-1:
+                print(list[count],list[count+1])
+            count += 1
+        return list
+    
+    
     
 newlist = cgroups(gsize,my_list)
+print (newlist)
